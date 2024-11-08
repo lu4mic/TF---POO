c19ff6cb3793bcfe0b9168a1c4de04051431f418
Bernardo Lykawka <bernardolmsilvas@gmail.com>
2024-11-08 19:30:20 -0300

    janela Drone implementada

--- src/janelas/DroneCargaForm.java
+++ src/janelas/DroneCargaForm.java
@@ -1,6 +1,8 @@
 package janelas;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class DroneCargaForm extends JFrame {
     private JPanel painelPrincipal;
@@ -11,6 +13,7 @@ public class DroneCargaForm extends JFrame {
     private JCheckBox protecaoCheckBox;
     private JCheckBox climatizacaoCheckBox;
     private JButton botaoEnviar;
+    private JLabel textoErro;
 
     public DroneCargaForm() {
         setTitle("Carga Form");
@@ -18,5 +21,32 @@ public class DroneCargaForm extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(720, 480);
         setVisible(true);
+        botaoEnviar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    if (cargaVivaCheckBox.isSelected() && protecaoCheckBox.isSelected()) {
+                        textoErro.setText("A carga viva nao deve ter proteção");
+                    }
+                    if (!cargaVivaCheckBox.isSelected() && climatizacaoCheckBox.isSelected()) {
+                        textoErro.setText("A carga inanimada nao deve ter climatizacao");
+                    }
+                    if (textoAutonomia.getText().isBlank() || textoCustoFixo.getText().isBlank() || textoPesoMax.getText().isBlank()) {
+                        textoErro.setText("Todos os campos devem ser preenchidos!");
+                    } else {
+                        textoErro.setText("");
+                        double autonomia = Double.parseDouble(textoAutonomia.getText());
+                        double custoFixo = Double.parseDouble(textoCustoFixo.getText());
+                        double pesoMax = Double.parseDouble(textoPesoMax.getText());
+                        boolean protecao = protecaoCheckBox.isSelected();
+                        boolean climatizacao = climatizacaoCheckBox.isSelected();
+                        boolean cargaViva = cargaVivaCheckBox.isSelected();
+                    }
+                }
+                catch (NumberFormatException ex) {
+                    textoErro.setText("Os campos devem ser preenchidos com numeros, utilize . para as casas decimais");
+                }
+            }
+        });
     }
 }
